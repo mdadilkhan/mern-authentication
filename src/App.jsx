68c907114db0ea505dfd196c681ba57fcdbc8b33
Md Adil Khan <md.adil.khan.@longshortlabs.com>
2023-12-30 22:03:08 +0530

    created header compoenent

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
-
-function App() {
+import Header from './components/Header'
+import About from './pages/About'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+const App=()=> {
 
 
   return (
     <>
-      <div className=''>Mern Auth hell</div>
+     <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/singup' element={<SignUp/>}/>
+        <Route path='/singin' element={<Signin/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+     </BrowserRouter>
     </>
   )
 }
