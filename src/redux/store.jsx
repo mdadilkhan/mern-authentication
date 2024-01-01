cf8346fdbef7bcd7f47b9185b949f1ffe62ca53f
Md Adil Khan <md.adil.khan.@longshortlabs.com>
2024-01-01 23:22:43 +0530

    added redux persist and blacklist and white list

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
 import userReducer from './slices/userSlice'
+import storage from 'redux-persist/lib/storage'
+import {persistReducer,persistStore} from 'redux-persist'
+
+
+
+const rootReducer=combineReducers({
+  user:userReducer,
+  // suppose we have many reducer u can add here using key value after the comma
+})
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+
+  // if u dont want som reducer to be in persistStore do blacklist or do white list or dont do whitelist simply write the reducer name
+  // blacklist: ['user'], 
+  // whitelist: ['user']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    user:userReducer
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddlware)=> getDefaultMiddlware({
     serializableCheck:false
   }),
 })
+
+
+
+export const persistor=persistStore(store)
